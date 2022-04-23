039a613a516b259cbb84b6f6c9d7834b0362a179
pawinee031 <pawinee.suksai.pang@gmail.com>
2022-04-23 19:36:27 +0700

    commit5-index

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -16,11 +16,14 @@ export default function Home({ token }) {
           <div class="flex flex-row justify-center border-2 ">
             <div class="w-6/12 sm:w-4/12 px-4 ">
             <a class="text-2xl text-red-600">KANIVA</a><br></br>
+            <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+        <img src="https://backend.tops.co.th/media/catalog/product/8/8/8850127004908_14-03-2022.jpg" class="w-full w-full object-center object-cover group-hover:opacity-75" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." />
+    </div>
             <a class="text-xl">Kanivaมีทั้งหมด 3 สูตร</a>
             
             </div>
             <ul class="list-disc">
-  <li>ต้องบอกว่าสำหรับทาสแมวนอกจากต้องดูแลทำความสะอาดที่จะต้องมีห้องน้ำแมวหรืออุปกรณ์ต่างๆ แต่วันนี้สาระทีมจะมาแนะนำอาหารแมวยอดฮิตกินแล้ว</li><br></br>
+  <li>ต้องบอกว่าสำหรับทาสแมวนอกจากต้องดูแลทำความสะอาดที่จะต้องมีห้องน้ำแมวหรืออุปกรณ์ต่างๆ แต่วันนี้สาระทีมจะมาแนะนำอาหารแมวยอดฮิตกินแล้ว</li>
     <li>สุขภาพดี ขนไม่ร่วง ขนนุ่ม ฟู สวยงาม เรามาดูกันว่ามีอะไรบ้าง</li>
 
 </ul>
